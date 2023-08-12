200e04d178c5d6fc9a85913623865213bfb7ffdc
Pratham131 <prathamakhand2000@gmail.com>
2023-08-13 00:55:57 +0530

    update2

--- Contest Answers/Frontend 3 Contest 1/script.js
+++ Contest Answers/Frontend 3 Contest 1/script.js
@@ -1,33 +1,63 @@
 // const url = "http://www.omdbapi.com/?i=tt3896198&apikey=3b8777fd"
 const container = document.getElementById("movieContainer");
-let arr = ["Fast and Furious", "Iron Man", "Thor", "Captain America", "Hulk", "Transformers", "Avengers" ]
+let arr = ["Fast and Furious", "Iron Man", "Thor", "Captain America", "Hulk", "Transformers", "Avengers", "Up" ]
 const apiKey = "3b8777fd";
+const loader = document.getElementById("loader");
 
 // Onload Data will be load
 window.addEventListener("load", () => {
-    
+    // loader.style.display = "block";
     arr.forEach(function (movie) {
-        fetchData(movie);
+        fetchData(movie, apiKey);
     })
 })
 
+const apiBtn = document.getElementById("apiBtn");
+apiBtn.addEventListener("click", () => {
+    let newApiKey = document.getElementById("newApiKey").value;
+    console.log(newApiKey);
+    arr.forEach(function (movie) {
+        fetchData(movie, newApiKey);
+    })
+})
 
-async function fetchData(moive) {
+const searchBtn = document.getElementById("searchBtn");
+searchBtn.addEventListener("click", () => {
+    let newMovie = document.getElementById("searchInput").value;
+    console.log(newMovie);
+    fetchData(newMovie, apiKey);
+})
+
+async function fetchData(moive, apiKey) {
     try {
         let url = `https://www.omdbapi.com/?t=${moive}&apikey=${apiKey}`;
         const response = await fetch(url);
         const result = await response.json();
 
-        // console.log(result);
-        // console.log(result.Genre);
-        addMoviesData(result);
+        
+        if(result.Response === 'False'){
+            console.log(response)
+
+        }
+        else{
+            console.log(result);
+            // console.log(result.Type);
+            
+            loader.style.display = "none";
+            addMoviesData(result);
+        }
+        
     }
     catch (error) {
         console.error("Invalid API Key!", error)
     }
 }
 
+
+
 function addMoviesData(data){
+
+    
 
     let card = document.createElement("div");
     card.className = "card";
@@ -40,8 +70,8 @@ function addMoviesData(data){
         <p>${data.Title}</p>
         <h4>Genre: ${data.Genre}</h4>
         <div class="type">
-            <span>Release: ${data.Relesed}</span>
-            <span>${data.Type}</span>
+            <span>Release: ${data.Released}</span>
+            <span>${capitalizeFirstLetter(data.Type)}</span>
         </div>
         <div class="rating">
             <span>ImdbId: ${data.imdbID}</span>
@@ -53,4 +83,10 @@ function addMoviesData(data){
     container.appendChild(card);
 }
 
-fetchData();
+function capitalizeFirstLetter(str) {
+    if (typeof str !== 'string' || str.length === 0) {
+        return str;
+    }
+
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
