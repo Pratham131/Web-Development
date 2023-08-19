285be13d8177bf93ba0569e1b32614ad1ae2e4bf
Pratham131 <prathamakhand2000@gmail.com>
2023-08-20 00:03:29 +0530

    Update Contest 3

--- Contest Answers/Frontend 3 Contest 2/script2.js
+++ Contest Answers/Frontend 3 Contest 2/script2.js
@@ -1,46 +1,33 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     const urlParams = new URLSearchParams(window.location.search);
     const ipAddress = urlParams.get("ip");
     const ipDisplay = document.getElementById("ip");
     ipDisplay.textContent = ipAddress;
 
-    // const IP = document.getElementById("ip").textContent
-    fetchData(ipDisplay.textContent)
-    console.log(Date().toString())
-})
-
-async function fetchData(ip){
-    
-    try{
-
-        const url = `http://ipinfo.io/${ip}?token=9fb465a2a7ec8c`
-        const response = await fetch(url);
-        const result = await response.json();
-
-        console.log(result)
-        addDetails(result, ip);
-
-        // const request = await fetch("https://ipinfo.io/json?token=9fb465a2a7ec8c")
-        // const jsonResponse = await request.json()
-
-        // console.log(jsonResponse.ip, jsonResponse)
-    } catch (error) {
-        console.log(error)
-    }
-
-    
-}
+    fetchData(ipDisplay.textContent);
+});
 
 let home = document.getElementById("home");
 
-function addDetails(data){
+async function fetchData(ip) {
+    try {
+        const url = `https://ipinfo.io/${ip}?token=9fb465a2a7ec8c`;
+        const response = await fetch(url);
+        const result = await response.json();
+
+        addDetails(result, ip);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function addDetails(data) {
+    postDate(data.postal);
 
     const section1 = document.getElementById("section1");
-
     const parts = data.loc.split(",");
 
-    section1.innerHTML = 
-    `<p>IP address is <span style="color: var(--white);" id="ip">${data.ip}</span></p>
+    section1.innerHTML = `<p>IP address is <span style="color: var(--white);" id="ip">${data.ip}</span></p>
     <table>
         <tbody>
             <tr>
@@ -51,32 +38,57 @@ function addDetails(data){
             <tr>
                 <td>Long: ${parts[1]}</td>
                 <td>Region: ${data.region}</td>
-                <td>Hostname: </td>
+                <td>Hostname:</td>
             </tr>
         </tbody>
-    </table>`
-    
-    // home.appendChild(section1)
+    </table>`;
 
-    const section2 = document.getElementById("section2")
+    const section2 = document.getElementById("section2");
 
-    section2.innerHTML= 
-    `<h1>Your Current Location</h1>
-    <iframe src="https://maps.google.com/maps?q=${data.loc}&z=15&output=embed" frameborder="0" style="border:0"></iframe>`
+    section2.innerHTML = `<h1>Your Current Location</h1>
+    <iframe src="https://maps.google.com/maps?q=${data.loc}&z=15&output=embed" frameborder="0" style="border:0"></iframe>`;
 
-    // home.appendChild(section2)
+    const section3 = document.getElementById("section3");
 
-    const section3 = document.getElementById("section3")
-    // const section3 = document.createElement("div");
-    // section3.id = "section3";
     const current = new Date();
 
-    section3.innerHTML=
-    `<h1>More Information About You</h1>
+    section3.innerHTML = `<h1>More Information About You</h1>
     <p>Time Zone: ${data.timezone}</p>
-    <p>Date and Time: ${current.toLocaleDateString() +" " +current.toLocaleTimeString()}</p>
+    <p>Date and Time: ${current.toLocaleDateString() + " " + current.toLocaleTimeString()
+        }</p>
     <p>Pincode: ${data.postal}</p>
-    <p>Message: Numvber of pincode found</p>`
+    <p>Message: Numvber of pincode found</p>`;
+}
 
-    // home.appendChild
+async function postDate(pincode) {
+    try {
+        let url = `https://api.postalpincode.in/pincode/${pincode}`;
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result[0]);
+
+        // const message = document.getElementById("mesa");
+        // message.textContent = `${result[0].Message}`
+        addPostData(result[0].PostOffice);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function addPostData(pData) {
+    const container = document.getElementById("container");
+
+    pData.forEach((ele) => {
+        console.log(ele.Name)
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `<p>Name: ${ele.Name}</p>
+        <p>Branch Type: ${ele.BranchType}</p>
+        <p>Delivery Status: ${ele.DeliveryStatus}</p>
+        <p>District: ${ele.District}</p>
+        <p>Division: ${ele.Division}</p>`
+
+        container.appendChild(card);
+    });
 }
